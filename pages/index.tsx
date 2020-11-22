6196c7f01b245fddf7f0fbd295f7f98908711db8
Blake Wilson <blake@blake.id>
2020-11-22 14:40:38 -0800

    Add about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Head from "next/head";
 import { FC } from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import PostsList from "../components/postsList";
 import { getPostMetas } from "../lib/api";
+import { siteName } from "../lib/constants";
 import { Post } from "../types";
 
 type HomeProps = {
@@ -12,6 +14,11 @@ type HomeProps = {
 const Home: FC<HomeProps> = ({ posts }) => {
   return (
     <div className="bg-white dark:bg-black">
+      <Head>
+        <title>All posts | {siteName}</title>
+
+        {/* TODO: SEO */}
+      </Head>
       <Header />
 
       <div className="relative max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
